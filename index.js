3d83ac9e90e8533022b5497fad2061f2906c7b08
Arthur Menezes <arthur.menezes89@gmail.com>
2019-11-01 17:05:39 -0300

    -create and check tasks

--- index.js
+++ index.js
@@ -9,9 +9,23 @@ chrome.setDefaultService(service);
         .withCapabilities(webdriver.Capabilities.chrome())
         .build();
     try {
-        await driver.get('http://www.google.com/ncr');
-        await driver.findElement(webdriver.By.name('q')).sendKeys('webdriver', webdriver.Key.RETURN);
-        await driver.wait(webdriver.until.titleIs('webdriver - Google Search'), 1000);
+        await driver.get('http://todomvc.com/examples/angular2/');
+        await driver.wait(webdriver.until.elementLocated(webdriver.By.css('input'))); // check if the page is rendered
+
+        // create tasks
+        await driver.findElement(webdriver.By.css("input")).sendKeys('task1', webdriver.Key.RETURN);
+        await driver.findElement(webdriver.By.css("input")).sendKeys('task2', webdriver.Key.RETURN);
+        await driver.findElement(webdriver.By.css("input")).sendKeys('task3', webdriver.Key.RETURN);
+        await driver.findElement(webdriver.By.css("input")).sendKeys('task4', webdriver.Key.RETURN);
+
+        // check the checkbox in the tasks
+        let taskListElements = await driver.findElements(webdriver.By.className('view'));
+        for (const tasksElement of taskListElements) {
+            await tasksElement.findElement({ className: 'toggle' }).click();
+        }
+
+
+        console.log(taskListElements);
     } catch (err) {
         console.error(err);
     }
